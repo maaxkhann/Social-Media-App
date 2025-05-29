0810fe134bf5e293ba5fd410f0d08ddb18984f09
Maaz Khan <maazkk514@gmail.com>
2025-05-29 18:02:48 +0500

    groups tab UI done

--- lib/views/chat/messages_view.dart
+++ lib/views/chat/messages_view.dart
@@ -5,6 +5,7 @@ import 'package:social_media/constants/app_text.dart';
 import 'package:social_media/controllers/profile_controller.dart';
 import 'package:social_media/extensions/sized_box.dart';
 import 'package:social_media/views/chat/widgets/chat_appbar.dart';
+import 'package:social_media/views/chat/widgets/group_widget.dart';
 import 'package:social_media/views/chat/widgets/messages_widget.dart';
 
 class MessagesView extends StatefulWidget {
@@ -42,22 +43,28 @@ class _MessagesViewState extends State<MessagesView>
           ),
         ),
       ),
-      body: TabBarView(
-        controller: tabController,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 15, right: 8, bottom: 15),
-            child: ListView.separated(
+      body: Padding(
+        padding: EdgeInsets.only(top: 20, left: 15, right: 8, bottom: 15),
+        child: TabBarView(
+          controller: tabController,
+          children: [
+            ListView.separated(
               itemCount: 10,
               separatorBuilder: (context, index) => 18.spaceY,
               itemBuilder: (context, index) {
                 return MessagesWidget();
               },
             ),
-          ),
-          Center(child: CustomText(title: 'Groups')),
-          Center(child: CustomText(title: 'Stories')),
-        ],
+            ListView.separated(
+              itemCount: 10,
+              separatorBuilder: (context, index) => 18.spaceY,
+              itemBuilder: (context, index) {
+                return GroupWidget();
+              },
+            ),
+            Center(child: CustomText(title: 'Stories')),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/chat/widgets/group_widget.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:social_media/constants/app_colors.dart';
+import 'package:social_media/constants/app_text.dart';
+import 'package:social_media/extensions/sized_box.dart';
+
+class GroupWidget extends StatelessWidget {
+  const GroupWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  foregroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  ),
+                ),
+
+                Positioned(
+                  bottom: 5,
+                  right: 1,
+                  child: CircleAvatar(
+                    radius: 6,
+                    backgroundColor: AppColors.yellow,
+                  ),
+                ),
+              ],
+            ),
+            12.spaceX,
+            Expanded(
+              flex: 8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    title: 'Buisness Ideas',
+                    size: 13,
+                    fontWeight: FontWeight.w800,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        title: 'Rashid: ',
+                        size: 13,
+                        fontWeight: FontWeight.w800,
+                      ),
+                      Expanded(
+                        child: CustomText(
+                          title:
+                              'Hi Iike your work which is very amazinI want to know more...',
+                          size: 11,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Column(
+              children: [
+                CustomText(
+                  title: '10:44 PM',
+                  size: 10,
+                  color: AppColors.yellow,
+                  fontWeight: FontWeight.w700,
+                ),
+                5.spaceY,
+                CircleAvatar(
+                  radius: 10,
+                  backgroundColor: AppColors.yellow,
+                  child: CustomText(
+                    title: '1',
+                    size: 12,
+                    color: AppColors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
